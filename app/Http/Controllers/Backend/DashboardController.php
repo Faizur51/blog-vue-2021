b6e9ee62032109bd65de772fb5c2b392e21dc0e0
Faizur Rahman <61926823+Faizur51@users.noreply.github.com>
2021-05-17 18:24:25 +0600

    test

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('backend/layout');
+        return view('backend/components/layout');
     }
 }
